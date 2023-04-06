6ba1e4def41b5500ad46dd539198c0c7cf01ca26
Sagi Dana <sagidana1@gmail.com>
2023-04-06 16:49:22 +0300

    transfer to package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(  name='moida',
+        version='1.0.0',
+        description='',
+        author='None',
+        author_mail='',
+        url='',
+        packages=['moida']
+        )
